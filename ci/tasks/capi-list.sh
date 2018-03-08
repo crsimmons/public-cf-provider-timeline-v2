27ee3f7d63e95cbf95d8b5ec0693edb7756ca738
Colin Simmons <colin.simmons@engineerbetter.com>
2018-03-08 22:56:02 +0000

    get the version and date of capi releases

--- ci/tasks/capi-list.sh
+++ ci/tasks/capi-list.sh
@@ -2,4 +2,16 @@
 
 set -eux
 
-cat capi-release/body
+tag=$(cat capi-release/tag)
+
+version=$(
+  grep -F '**CC API Version:' capi-release/body \
+  | grep -oE "2\.[0-9]+\.[0-9]+"
+)
+
+date=$(
+  curl -s -H "Authorization: token ${API_TOKEN}" "https://api.github.com/repos/cloudfoundry/capi-release/releases/tags/${tag}" \
+  | jq -r '.published_at'
+)
+
+echo "${version},${date}"
